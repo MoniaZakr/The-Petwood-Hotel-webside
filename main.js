e79935ee96f462dc7e823b847c305ede7be8f18f
Moika Zakrzewska <monikawiniar@gmail.com>
2022-05-05 18:24:26 +0200

    added function that makes the appropriate picture appear after pressing the navigation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const navigation = [...document.querySelectorAll("li")];
+const navigation = [...document.querySelectorAll(".navigation li")];
+const pictures = [...document.querySelectorAll(".pictures img")]
 
 for (var i = 0; i < navigation.length; i++) {
     navigation[i].addEventListener( "mouseover", toggle);
@@ -11,7 +12,43 @@ function toggle() {
         item.classList.add("disabled")
     );
     li.classList.remove("disabled");
+    li.classList.add("clikedElement");
+    
+    if(li === navigation[0]) {
+        pictures[0].classList.add("active")
+    }
+    
+    else if(li === navigation[1]) {
+        pictures[1].classList.add("active")
+    }
+    else if(li === navigation[2]) {
+        pictures[2].classList.add("active")
+    }
+    else if(li === navigation[3]) {
+        pictures[3].classList.add("active")
+    }else if(li === navigation[4]) {
+        pictures[4].classList.add("active")}
 }
+
 function exit() {
-    navigation.forEach(item => item.classList.remove("disabled"))
+    navigation.forEach(item => {
+        item.classList.remove("disabled");
+        item.classList.remove("clikedElement")
+    })
+    const li = this;
+    if(li === navigation[0]) {
+        pictures[0].classList.remove("active")
+    }
+    else if(li === navigation[1]) {
+        pictures[1].classList.remove("active")
+    }
+    else if(li === navigation[2]) {
+        pictures[2].classList.remove("active")
+    }
+    else if(li === navigation[3]) {
+        pictures[3].classList.remove("active")
+    }
+    else if(li === navigation[4]) {
+        pictures[4].classList.remove("active")
+    }
 }
