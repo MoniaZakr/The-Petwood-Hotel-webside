e85e696c55480a77fdd2c1c5e1e621a5c3e20fc0
Moika Zakrzewska <monikawiniar@gmail.com>
2022-08-09 09:44:05 +0200

    Logo and logo button created in weedings and relax folders

---
+++ scripts/weedings.js
@@ -0,0 +1,46 @@
+const pictures = document.querySelector(".pictures");
+const images = document.querySelectorAll(".pictures .wedding");
+ let active = 0;
+
+
+let changeBaner = setInterval(() => {
+    switch(active) {
+        case 0: {
+           images[0].style.visibility = 'visible';
+           images[1].style.visibility = 'hidden';
+           images[2].style.visibility = 'hidden';
+           images[3].style.visibility = 'hidden';
+
+           active++;
+           break;
+        }
+        case 1: {
+           images[0].style.visibility = 'hidden';
+           images[1].style.visibility = 'visible';
+           images[2].style.visibility = 'hidden';
+           images[3].style.visibility = 'hidden';
+
+           active++;
+           break;
+        }
+        case 2: {
+           images[0].style.visibility = 'hidden';
+           images[1].style.visibility = 'hidden';
+           images[2].style.visibility = 'visible';
+           images[3].style.visibility = 'hidden';
+
+           active++;
+           break;
+         }
+         case 3: {
+            images[0].style.visibility = 'hidden';
+            images[1].style.visibility = 'hidden';
+            images[2].style.visibility = 'hidden';
+            images[3].style.visibility = 'visible';
+ 
+            active = 0;
+            break;
+          }
+      }
+},2500);
+
